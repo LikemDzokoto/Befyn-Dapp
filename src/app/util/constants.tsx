1570e9d60a750a8714af7a3df9780cd4551d35a3
LikemDzokoto <likemdzokoto@gmail.com>
2024-06-18 20:46:49 +0000

    update reaupdate readme

---
+++ src/app/util/constants.tsx
@@ -0,0 +1,3 @@
+export const CONTRACT_ADDRESS = "";
+export const CHARITY_REGISTRY_CONTRACT_ABI = ;
+export const CHAIN_ID = "0x5"; //
---
+++ src/app/util/helpers.tsx
@@ -0,0 +1,4 @@
+export function compressAddress(address: string): string {
+    return address.slice(0, 5) + "..." + address.slice(-4);
+  }
+  
