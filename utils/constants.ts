ff25036908a2d7b4df672830d4ac6e35a203fd22
LikemDzokoto <likemdzokoto@gmail.com>
2024-06-21 08:40:08 +0000

    fix: parse abi error

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,5 @@
 
 import abi from "../abis/Insurance.json";
 export const CONTRACT_ADDRESS = "0x6777B22b55286025A5a48d970772811AC3bAD1a3";
-// export const CONTRACT_ABI = abi.abi;
 export const CONTRACT_ABI = abi[0].abi;
 export const CHAIN_ID = "0xaa36a7"; // sepolia Testnet
